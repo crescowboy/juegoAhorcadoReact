457bfd058cd4d3405923b708ed2d1af8ae6427d1
wcrespo <wellingtoncrespo2001@gmail.com>
2023-04-24 12:59:59 -0500

    cambios2

--- src/componentes/Juego.jsx
+++ src/componentes/Juego.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { PALABROS } from '../assets/palabros'
 
 const Juego = () => {
+    const [azar,setAzar] = useState(0);
+    const [palabra,setPalabra] = useState([]);
+    const [misLetras,setMisLetras] = useState([])
+    useEffect(()=>{
+        setAzar(Math.floor(Math.random()*PALABROS.length))
+    },[])
     
+    useEffect(()=>{
+        setPalabra(PALABROS[azar].palabro.split(""))
+    },[azar])
+
   return (
     <>
-      juego
+      <div className='pregunta'>{PALABROS[azar].pregunta}</div>
+      <div className='palabra'>
+        {palabra.map((letra,i)=>(
+            misLetras.indexOf(letra)===-1
+            ?
+                <div className='palo' key={i}></div>
+            : 
+                <div className='palo' key={i}>{letra.toUpperCase()}</div>
+        ))}
+      </div>
+      <div className='botones'></div>
+      <div className='imagen'></div>
     </>
   )
 }
